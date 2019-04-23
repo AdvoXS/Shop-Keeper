6b827e5f3e0f584027a37a25576b9f53d618767b
Ivan Borisov <tvadmix@gmail.com>
2019-04-23 20:19:34 +0300

    v 0.35

--- ShopKeeper/WindowLeksi.xaml.cs
+++ ShopKeeper/WindowLeksi.xaml.cs
@@ -20,12 +20,72 @@ namespace ShopKeeper
     public partial class WindowLeksi : Window
     {
         Window parentForm;
+        Dictionary<int, Grid> gridStatus;
         public WindowLeksi(Window parentWindow)
         {
             InitializeComponent();
             parentForm = parentWindow;
-        }
+            gridStatus = new Dictionary<int, Grid>();
+            gridStatus.Add(1, multiGrid);
+            gridStatus.Add(2, speedGrid);
+            gridStatus.Add(3, gameGrid);
 
+            multiBtnDown.Click += buttonDown_Click;
+            speedBtnDown.Click+= buttonDown_Click;
+            gameBtnDown.Click+= buttonDown_Click;
+
+            multiBtnUp.Click += buttonUp_Click;
+            speedBtnUp.Click += buttonUp_Click;
+            gameBtnUp.Click += buttonUp_Click;
+        }
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            Button but = sender as Button;
+            for(int i = 0; i < gridStatus.Count; i++)
+            {
+                if (gridStatus.ElementAt(i).Value == but.Parent)
+                {
+                    if (gridStatus.ElementAt(i).Key != gridStatus.Keys.Max())
+                    {
+
+
+                        Grid n = gridStatus.ElementAt(i).Value;
+                        gridStatus[gridStatus.ElementAt(i).Key] = gridStatus.ElementAt(i + 1).Value;
+                        gridStatus[gridStatus.ElementAt(i+1).Key] = n;
+                        mainStackPanel.Children.Clear();
+                        for(int j=0;j < gridStatus.Count; j++)
+                        {
+                            mainStackPanel.Children.Add(gridStatus.ElementAt(j).Value);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            Button but = sender as Button;
+            for (int i = 0; i < gridStatus.Count; i++)
+            {
+                if (gridStatus.ElementAt(i).Value == but.Parent)
+                {
+                    if (gridStatus.ElementAt(i).Key != gridStatus.Keys.Min())
+                    {
+
+
+                        Grid n = gridStatus.ElementAt(i-1).Value;
+                        gridStatus[gridStatus.ElementAt(i-1).Key] = gridStatus.ElementAt(i).Value;
+                        gridStatus[gridStatus.ElementAt(i).Key] = n;
+                        mainStackPanel.Children.Clear();
+                        for (int j = 0; j < gridStatus.Count; j++)
+                        {
+                            mainStackPanel.Children.Add(gridStatus.ElementAt(j).Value);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
         private void WinLeksi_Closed(object sender, EventArgs e)
         {
 
